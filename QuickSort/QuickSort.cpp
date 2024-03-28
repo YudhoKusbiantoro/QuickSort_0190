149fe3690c4e7b9c4b0e29a7693734514de56766
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-03-28 10:03:58 +0700

    prosedur swap

--- QuickSort/QuickSort.cpp
+++ QuickSort/QuickSort.cpp
@@ -33,4 +33,13 @@ void input() {
 		cin >> arr[i];
 	}
 
+} 
+//swap the element at index x with the element at index y
+void swap(int x, int y)
+{
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+	mov_count++;
+
 }
